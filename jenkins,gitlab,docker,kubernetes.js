ed2f20172b81331966a0f8bdbe7fb0b2a3b5fbc1
rancheng <rancheng@zjwg.com>
2021-03-08 13:58:33 +0800

    version1

---
+++ jenkins,gitlab,docker,kubernetes.js
@@ -0,0 +1,11 @@
+// 参考文章 https://blog.csdn.net/lyfqyr/article/details/84068063
+
+1. 找运维要到 jenkins 地址， 用户名，密码
+
+2. 打开jenkins 创建一个新任务
+    1. 填写任务名称
+
+
+
+https://jenkins.lanhuapp.com/project/lanhu-ea-web
+    48806f7d78db337b075e723ec5c858ea
